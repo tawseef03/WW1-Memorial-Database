fcd9ad25487e88d91631adfdcfe39593eb274841
MoJawad123 <mo.jawad.004@gmail.com>
2025-02-18 14:43:12 +0000

    Update Log-in.php

--- WW1-Code/php:html/Log-in.php
+++ WW1-Code/php:html/Log-in.php
@@ -11,4 +11,71 @@
 
     </head>
 
+<body>
+
+
+    
+        <div class="header header-width">
+            <div class="container">
+        <div class="navbar navbar-backlogin">
+            <div class="logo">
+                <img src="images/Logo.png" width="200px">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="products.php">Products</a></li>
+                    <li><a href="about.html">About</a></about></li>
+                    <li><a href="contactus.html">Contact Us</a></li>
+                    <li><a href="">Account</a></li>
+                </ul>
+            </nav>
+            <img src="images/cart1.png" width="30px" height="30px">
+        </div> 
+    </div>
+    </div>
+
+
+    <div class="wrapper">
+        <form method="post">
+            <h1> Log-in</h1>
+
+            <?php if ($is_invalid): ?>
+                <div style="text-align: center; color: red;, margin-bottom: 50px:">
+                  <em> Invalid Login</em>
+                </div>
+            <?php endif; ?>
+
+            <div class="input-box">
+                <label for="email"></label>
+                <input type="email" name="email"  id="email" placeholder="Enter Your Email" required
+                        value ="<?= htmlspecialchars($_POST["email"] ?? "") ?>" >
+                <i class='bx bxs-user-circle' ></i>
+            </div>
+            <div class="input-box">
+                <label for="password"></label>
+                <input type="password" name="password" id="password" placeholder="Enter Your Password" required>
+                <i class='bx bxs-lock' ></i>
+            </div>
+
+            <div class="rememberforgot">
+                <label><input type="checkbox"> Remember Me </label>
+                <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" class="button">Log-in</button>
+
+            <div class="Register-link">
+                <p>Don't have an Account? 
+                    <a href="register.html">Register Here</a>
+                </p>
+
+            </div>
+
+
+        </form>
+    </div>
+
+</body>
+</html>
+
     
