4f3a537ea63fc2d8c2695f45045175ff15b9c2ec
thisispossiable <765601456@qq.com>
2025-02-24 17:22:32 +0000

    fix something

--- WW1-Code/php_html/Log-in.php
+++ WW1-Code/php_html/Log-in.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,55 +8,48 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="loginstyle.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-
-    </head>
-
+</head>
 <body>
-
-
-    
-        <div class="header header-width">
-            <div class="container">
-        <div class="navbar navbar-backlogin">
-            <div class="logo">
-                <img src="images/Logo.png" width="200px">
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="products.php">Products</a></li>
-                    <li><a href="about.html">About</a></about></li>
-                    <li><a href="contactus.html">Contact Us</a></li>
-                    <li><a href="">Account</a></li>
-                </ul>
-            </nav>
-            <img src="images/cart1.png" width="30px" height="30px">
-        </div> 
+    <div class="header header-width">
+        <div class="container">
+            <div class="navbar navbar-backlogin">
+                <div class="logo">
+                    <img src="images/Logo.png" width="200px">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="products.php">Products</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contactus.html">Contact Us</a></li>
+                        <li>
+                    </ul>
+                </nav>
+                <img src="images/cart1.png" width="30px" height="30px">
+            </div> 
+        </div>
     </div>
-    </div>
-
 
     <div class="wrapper">
         <form method="post">
             <h1> Log-in</h1>
 
             <?php if ($is_invalid): ?>
-                <div style="text-align: center; color: red;, margin-bottom: 50px:">
+                <div style="text-align: center; color: red; margin-bottom: 50px;">
                   <em> Invalid Login</em>
                 </div>
             <?php endif; ?>
 
             <div class="input-box">
                 <label for="email"></label>
-                <input type="email" name="email"  id="email" placeholder="Enter Your Email" required
-                        value ="<?= htmlspecialchars($_POST["email"] ?? "") ?>" >
-                <i class='bx bxs-user-circle' ></i>
+                <input type="email" name="email" id="email" placeholder="Enter Your Email" required
+                       value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                <i class='bx bxs-user-circle'></i>
             </div>
             <div class="input-box">
                 <label for="password"></label>
                 <input type="password" name="password" id="password" placeholder="Enter Your Password" required>
-                <i class='bx bxs-lock' ></i>
+                <i class='bx bxs-lock'></i>
             </div>
 
             <div class="rememberforgot">
@@ -68,14 +62,8 @@
                 <p>Don't have an Account? 
                     <a href="register.html">Register Here</a>
                 </p>
-
             </div>
-
-
         </form>
     </div>
-
 </body>
 </html>
-
-    
