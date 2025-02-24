f80d99238470ae65f554c2248e7f38185268ccbd
thisispossiable <765601456@qq.com>
2025-02-24 17:49:00 +0000

    fix log-in page
    its css still need to fix

--- WW1-Code/php_html/Log-in.php
+++ WW1-Code/php_html/Log-in.php
@@ -1,3 +1,14 @@
+<?php
+// 在使用 $is_invalid 之前先定义它
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 处理表单提交逻辑
+    // 如果登录失败，将 $is_invalid 设置为 true
+    // $is_invalid = true;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +16,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sports-Online Log-in</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="../css/loginstyle.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
@@ -14,18 +25,18 @@
         <div class="container">
             <div class="navbar navbar-backlogin">
                 <div class="logo">
-                    <img src="images/Logo.png" width="200px">
+                    <img src="../rsc/WebLogo.png" width="200px">
                 </div>
                 <nav>
                     <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="products.php">Products</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contactus.html">Contact Us</a></li>
-                        <li>
+                        <li><a href="../index.php">Home</a></li>
+                        <li><a href="../products.php">Products</a></li>
+                        <li><a href="../about.html">About</a></li>
+                        <li><a href="../contactus.html">Contact Us</a></li>
                     </ul>
                 </nav>
                 <img src="images/cart1.png" width="30px" height="30px">
+                <img src="../rsc/cart1.png" width="30px" height="30px">
             </div> 
         </div>
     </div>
@@ -60,7 +71,7 @@
 
             <div class="Register-link">
                 <p>Don't have an Account? 
-                    <a href="register.html">Register Here</a>
+                    <a href="../register.html">Register Here</a>
                 </p>
             </div>
         </form>
