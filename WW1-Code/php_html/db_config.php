929b17fdf3604c209b0fb77477d62111e86368b7
Zehao Yang <140780108+thisispossiable@users.noreply.github.com>
2025-03-04 22:00:30 +0000

    Update db_config.php

--- WW1-Code/php_html/db_config.php
+++ WW1-Code/php_html/db_config.php
@@ -1,8 +1,8 @@
 <?php
 $dbConfig = [
     'host' => 'localhost',
-    'username' => 'username',
-    'password' => 'password',
-    'dbname' => 'database'
+    'username' => 'root', // 默认用户名
+    'password' => '', // 初始密码为空
+    'dbname' => 'ww1_memorial_db'  // 数据库名称
 ];
 ?>
